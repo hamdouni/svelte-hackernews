74e891de17478883e391676557858a84dede8b03
Brahim Hamdouni <brahim@hamdouni.com>
2023-11-13 14:17:47 +0100

    refactor

--- app.svelte
+++ app.svelte
@@ -11,29 +11,25 @@
 
 	$: lasttime = last.toLocaleDateString() + " " + last.toLocaleTimeString();
 
-	function getpage(page) {
+	let getpage = (page) => {
 		news = [];
 		var url = `https://node-hnapi.herokuapp.com/` + tag + `?page=` + page;
 		fetch(url)
 			.then((r) => r.json())
-			.then((data) => {
-				news = data;
-			});
+			.then((data) => news = data );
 		last = new Date();
 	}
-	function changetag(t) {
+	let changetag = (t) => {
 		tag = t;
 		reload();
 	}
-	function next() {
-		page++;
-	}
-	function prev() {
+	let next = () => page++; 
+	let prev = () => {
 		if (page > 1) {
 			page--;
 		}
 	}
-	function reload() {
+	let reload = () => {
 		page = 1;
 		getpage(page);
 	}
@@ -83,6 +79,22 @@
 				<div class="toolbar">
 					<span>
 						Page {page}
+						<button on:click={reload}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+								/>
+							</svg>
+						</button>
 						<button on:click={prev}>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -112,22 +124,6 @@
 									stroke-linecap="round"
 									stroke-linejoin="round"
 									d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-								/>
-							</svg>
-						</button>
-						<button on:click={reload}>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
 								/>
 							</svg>
 						</button>
@@ -179,6 +175,10 @@
 		display: flex;
 		justify-content: end;
 		align-items: center;
+	}
+	footer > section > .toolbar button {
+		font-size: 1.5rem;
+		padding: 0.5rem 1.5rem;
 	}
 	main > section {
 		padding: 1.5rem 0;
